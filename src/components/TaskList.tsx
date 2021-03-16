7188d5e4df6f773da0dea8bc5e9f292533710aac
Italo Almeida <italoalmeida89@gmail.com>
2021-03-16 12:04:13 -0300

    adds logic in 'handleToggleTaskCompletion'

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -31,6 +31,13 @@ export function TaskList() {
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+
+    setTasks(tasks.map(task => {
+      return task.id == id ? {
+        ...task,
+        isComplete: !task.isComplete
+      } : task
+    }))
   }
 
   function handleRemoveTask(id: number) {
